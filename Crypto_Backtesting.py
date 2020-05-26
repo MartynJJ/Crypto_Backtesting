1b7d25af807f4e3d90caae65664e1e33e126061b
Martyn <16118416+MartynJJ@users.noreply.github.com>
2020-05-25 21:41:30 -0400

    updated with prototype

--- Crypto_Backtesting.py
+++ Crypto_Backtesting.py
@@ -1,5 +1,23 @@
 from bitmex import bitmex
+import json
+from datetime import datetime as dt
+import pandas as pd
+import warnings
+from swagger_spec_validator.common import SwaggerValidationWarning
+
+
+
 
 if __name__ == "__main__":
     print("Hello_World")
     REMOTE = False
+    warnings.simplefilter("ignore", SwaggerValidationWarning)
+
+
+
+client = bitmex(test=True)
+# test = client.Instrument(symbol="XBT", count=10)
+# test = pd.DataFrame(client.Trade.Trade_get(symbol="XBT", count=2, columns=["price"]).result()[0])
+test = client.Trade.Trade_get(symbol="XBT", count=200).result()[0]
+
+# print(dir(test))
